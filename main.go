fc83c03a76c75622a97a81c5b3dae13093afc1c9
fractalbach <4bach@protonmail.com>
2018-04-07 09:55:00 -0700

    actually fetches correctly now.

--- main.go
+++ main.go
@@ -1,57 +1,31 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"reflect"
 	"text/template"
+	"time"
+
+	"github.com/fractalbach/ninjatools/structInspector"
 )
 
-const verbose = true
+const version = "0.0.2"
 const endpoint = "https://api.github.com/graphql"
-
 const help = `
 SYNOPSIS:
   Download Latest Github Release using the GRAPHQL API.
 
 USAGE:
-  TestGoReleaser -repo <repoName> -owner <ownerName> [OPTIONS]...
+  TestGoReleaser -repo <repoName> -owner <ownerName> -token [OPTIONS]...
 
 `
 
-var Query = `
-query {
-  repository(owner: {{.Owner}}, name: {{.Repo}}) {
-    releases(last: 1) {
-      edges {
-        node {
-          publishedAt
-          tag {
-            name
-          }
-          releaseAssets(first: 10) {
-            edges {
-              node {
-                name
-                downloadUrl
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
-
-// var repo_owner = "fractalbach"
-// var repo_name  = "TestGoReleaser"
+var verbose = false
+var Query = `{"query":"{\n  repository(owner: \"{{.Owner}}\", name: \"{{.Repo}}\") {\n    releases(last: 1) {\n      edges {\n        node {\n          publishedAt\n          tag {\n            name\n          }\n          releaseAssets(first: 10) {\n            edges {\n              node {\n                name\n                downloadUrl\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n}\n","variables":"{}","operationName":null}`
 
 type Repo struct {
 	Owner, Repo string
@@ -78,163 +52,107 @@ func checkErrLogFatal(err error) {
 	}
 }
 
+func NewClient() *http.Client {
+	myTransport := &http.Transport{
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+	return &http.Client{
+		Timeout:   time.Second * 60,
+		Transport: myTransport,
+	}
+}
+
+func printReqRespInfo(req *http.Request, resp *http.Response) {
+	fmt.Println("\n ~~~~ Request ~~~~")
+	structInspector.PrettyPrint(req, 3)
+
+	fmt.Println("\n ~~~~ Request Headers ~~~~")
+	for key, val := range req.Header {
+		fmt.Printf("[%s] : %s\n", key, val)
+	}
+	fmt.Println("\n ~~~~ Response ~~~~")
+	structInspector.PrettyPrint(resp, 3)
+
+	fmt.Println("\n ~~~~ Response Headers ~~~~")
+	for key, val := range resp.Header {
+		fmt.Printf("[%s] : %s\n", key, val)
+	}
+}
+
+func doFetch(reader io.Reader, url, token string) *http.Response {
+	client := NewClient()
+	req, err := http.NewRequest("POST", url, reader)
+	checkErrLogFatal(err)
+	req.Header.Add("User-Agent", "Achenbot/"+version)
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+	req.Header.Add("Authorization", "bearer "+token)
+
+	resp, err := client.Do(req)
+	checkErrLogFatal(err)
+
+	if verbose {
+		printReqRespInfo(req, resp)
+	}
+
+	return resp
+}
+
 func main() {
 
-	// Flags
 	repoPtr := flag.String("repo", "", "Name of the github repository. (Required)")
 	ownerPtr := flag.String("owner", "", "Repository owner's name. (Required)")
+	tokenPtr := flag.String("token", "", "Github OAUTH2 Token Value. (Recommended)")
+	// TODO: add token path flag.
+	versionBool := flag.Bool("version", false, "Prints the version of the program.")
+	verboseBool := flag.Bool("verbose", false, "Prints headers and data structures of request and response.")
 	flag.Parse()
 
-	var repo string = *repoPtr
-	var owner string = *ownerPtr
+	repo := *repoPtr
+	owner := *ownerPtr
+	token := *tokenPtr
+	verbose = *verboseBool
 
-	// Display the help message if the required flags aren't filled.
+	// Handle the flags.  Print help menus if needed.
 	switch {
+	case *versionBool:
+		fmt.Println(version)
+		return
 	case repo == "":
 		ToHelp("Needs a repository name.")
 	case owner == "":
 		ToHelp("Need to have owner name.")
 	}
 
-	// Store the user input.
-	myRepo := Repo{Owner: owner, Repo: repo}
-
 	// Create message body template using the data structure fields.
+	myRepo := Repo{Owner: owner, Repo: repo}
 	tmpl, err := template.New("test").Parse(Query)
-	if err != nil {
-		panic(err)
-	}
+	checkErrPanic(err)
 
-	// Create a reader to store the message body.
-	var b bytes.Buffer
-	var body = bufio.NewReadWriter(bufio.NewReader(&b), bufio.NewWriter(&b))
+	// Create a pipe reader/writer for the message body.
+	r, w := io.Pipe()
 
 	// Write the template using the user's input.
-	err = tmpl.Execute(body, &myRepo)
-	checkErrPanic(err)
+	go func() {
+		err = tmpl.Execute(w, &myRepo)
+		checkErrPanic(err)
 
-	err = body.Flush()
-	checkErrPanic(err)
+		err = w.Close()
+		checkErrPanic(err)
+	}()
 
 	// The Fetcher!
-	resp, err := http.Post(endpoint, "application/json", body)
-	checkErrLogFatal(err)
+	resp := doFetch(r, endpoint, token)
 
-	// This is for looking at the headers and info about the request.
-	if verbose == true {
-		s := reflect.ValueOf(resp).Elem()
-		t := s.Type()
-		for i := 0; i < s.NumField(); i++ {
-			f := s.Field(i)
-			fmt.Fprintf(os.Stdout, "%d: %s %s = %v\n", i, t.Field(i).Name, f.Type(), f.Interface())
-		}
+	// copy response body to standard output
+	if verbose {
+		fmt.Println("\n ~~~~ Response Body ~~~~")
 	}
-
-	n, err := io.Copy(os.Stdout, resp.Body)
+	_, err = io.Copy(os.Stdout, resp.Body)
 	checkErrLogFatal(err)
-
-	fmt.Println(n)
-
 }
 
-// func Test() {
-// 	ownerArg := Arg("owner", "fractalbach")
-// 	nameArg := Arg("name", "TestGoReleaser")
-// 	last1 := Arg("last", 1)
-// }
+/* Potential GRAPHQL stuff.
 
-/*
-   Example Commands:
-   myRepo := repository{owner: "fractalbach", name: "TestGoReleaser"}
-*/
-
-/* Example Query
-
-
-{
-  repository(owner: "fractalbach", name: "TestGoReleaser") {
-    releases(last: 1) {
-      edges {
-        node {
-          publishedAt
-          tag {
-            name
-          }
-          releaseAssets(first: 10) {
-            edges {
-              node {
-                name
-                downloadUrl
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-*/
-
-/* Example Response
-
-
-{
-  "data": {
-    "repository": {
-      "releases": {
-        "edges": [
-          {
-            "node": {
-              "publishedAt": "2018-04-04T10:50:33Z",
-              "tag": {
-                "name": "HelloWorld_Build_39"
-              },
-              "releaseAssets": {
-                "edges": [
-                  {
-                    "node": {
-                      "name": "HelloWorld_windows_amd64.tar.gz",
-                      "downloadUrl": "https://github.com/fractalbach/TestGoReleaser/releases/download/HelloWorld_Build_39/HelloWorld_windows_amd64.tar.gz"
-                    }
-                  },
-                  {
-                    "node": {
-                      "name": "HelloWorld_linux_amd64.tar.gz",
-                      "downloadUrl": "https://github.com/fractalbach/TestGoReleaser/releases/download/HelloWorld_Build_39/HelloWorld_linux_amd64.tar.gz"
-                    }
-                  },
-                  {
-                    "node": {
-                      "name": "HelloWorld_darwin_amd64.tar.gz",
-                      "downloadUrl": "https://github.com/fractalbach/TestGoReleaser/releases/download/HelloWorld_Build_39/HelloWorld_darwin_amd64.tar.gz"
-                    }
-                  },
-                  {
-                    "node": {
-                      "name": "HelloWorld_linux_arm6.tar.gz",
-                      "downloadUrl": "https://github.com/fractalbach/TestGoReleaser/releases/download/HelloWorld_Build_39/HelloWorld_linux_arm6.tar.gz"
-                    }
-                  },
-                  {
-                    "node": {
-                      "name": "HelloWorld_linux_arm7.tar.gz",
-                      "downloadUrl": "https://github.com/fractalbach/TestGoReleaser/releases/download/HelloWorld_Build_39/HelloWorld_linux_arm7.tar.gz"
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        ]
-      }
-    }
-  }
-}
-
-*/
-
-/*
 // Means argument,
 type Arg struct {
     name string
